b69f88fa66de96996446136a1afa10499e3bbb54
Konstantin Burla <me@kburla.com>
2021-11-13 18:52:56 +0000

    moved to functions

--- dupefinder.py
+++ dupefinder.py
@@ -8,9 +8,8 @@ args = parser.parse_args()
 if args.folder:
     src_folder = args.folder
 
-
+# generate md5 hashes
 def generate_md5(filename, chunk_size=4096):
-
     hash = hashlib.md5()
     with open(filename, "rb") as f:
         while chunk := f.read(8192):
@@ -18,30 +17,37 @@ def generate_md5(filename, chunk_size=4096):
 
     return hash.hexdigest()
 
-if __name__ == "__main__":
-
-    files_dict = dict()
-
-    print("Generating checksums...")
-    for currentpath, folders, files in os.walk(src_folder):
-        for file in files:
-            fullpath = os.path.join(currentpath, file)
-            print(fullpath, end="....  ")
-            md5 = generate_md5(fullpath)
-            print(md5)
-            files_dict[fullpath] = md5
-
-    # show duplicates
+# create a set of dupes
+def create_dupeset(files_dict):
     dupeset = {}
-
-    # create a set of dupes
     for key, value in files_dict.items():
         if value not in dupeset:
             dupeset[value] = [key]
         else:
             dupeset[value].append(key)
+    return dupeset
 
-    # show only the dupes
+# get list of files
+def get_filelist(root_dir, verbose=False):
+    for currentpath, folders, files in os.walk(root_dir):
+        for file in files:
+            fullpath = os.path.join(currentpath, file)
+            if (verbose):
+                print(fullpath, end="....  ")
+            md5 = generate_md5(fullpath)
+            if (verbose):
+                print(md5)
+            files_dict[fullpath] = md5
+
+if __name__ == "__main__":
+
+    files_dict = dict()
+
+    print("Generating checksums...")
+    filelist = get_filelist(src_folder)
+    dupeset = create_dupeset(files_dict)
+
+    # show dupes
     print ("The following files are duplicates:")
     for key, value in dupeset.items():
         count = 0
