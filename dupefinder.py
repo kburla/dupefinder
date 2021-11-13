8c7d2eb94154a40e3e6ed18f995d99d6716c0258
Konstantin Burla <mail@kburla.com>
2021-11-13 14:06:07 -0500

    moved args, added verbosity

--- dupefinder.py
+++ dupefinder.py
@@ -2,12 +2,6 @@ import hashlib
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description="Get MD5 hash of files in folder")
-parser.add_argument("folder", type=str, help="enter folder name to check")
-args = parser.parse_args()
-if args.folder:
-    src_folder = args.folder
-
 # generate md5 hashes
 def generate_md5(filename, chunk_size=4096):
     hash = hashlib.md5()
@@ -32,19 +26,26 @@ def get_filelist(root_dir, verbose=False):
     for currentpath, folders, files in os.walk(root_dir):
         for file in files:
             fullpath = os.path.join(currentpath, file)
-            if (verbose):
-                print(fullpath, end="....  ")
             md5 = generate_md5(fullpath)
             if (verbose):
+                print(fullpath, end="....  ")
                 print(md5)
             files_dict[fullpath] = md5
 
 if __name__ == "__main__":
 
+    # arguments
+    parser = argparse.ArgumentParser(description="Get MD5 hash of files in folder")
+    parser.add_argument("folder", type=str, help="enter folder name to check")
+    parser.add_argument("-v", "--verbose", help="Show MD5 information", action="store_true")
+    args = parser.parse_args()
+    if args.folder:
+        src_folder = args.folder
+
     files_dict = dict()
 
     print("Generating checksums...")
-    filelist = get_filelist(src_folder)
+    filelist = get_filelist(src_folder, True if args.verbose else False)
     dupeset = create_dupeset(files_dict)
 
     # show dupes
